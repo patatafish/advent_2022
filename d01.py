834e4b2c2d22ac8d1babc0d6fc522a5cce6e77cc
Patrick Mendonca <mendonca.patrick@yahoo.com>
2022-12-02 17:27:38 +0800

    first commit
    2022 calendar

--- d01.py
+++ d01.py
@@ -1,6 +1,5 @@
 from shared_use import read_file
 
-
 def count_inventory(my_data):
     inventory = []
     elf_has = 0
@@ -25,7 +24,6 @@ def main():
     elf_food = count_inventory(raw_data)
     print(elf_food[0], elf_food[1], elf_food[2])
     print(elf_food[0] + elf_food[1] + elf_food[2])
-
 
 if __name__ == '__main__':
     print('Running day 1 directly')
--- d02.py
+++ d02.py
@@ -2,6 +2,58 @@ from shared_use import read_file
 
 
 def process(my_data):
+    total = 0
+
+    for line in my_data:
+        score = 0
+        [opp, me] = line.split(' ')
+
+        if me == 'X':
+            if opp == 'A':
+                me = 'Z'
+            if opp == 'B':
+                me = 'X'
+            if opp == 'C':
+                me = 'Y'
+        elif me == 'Y':
+            if opp == 'A':
+                me = 'X'
+            if opp == 'B':
+                me = 'Y'
+            if opp == 'C':
+                me = 'Z'
+        else:
+            if opp == 'A':
+                me = 'Y'
+            if opp == 'B':
+                me = 'Z'
+            if opp == 'C':
+                me = 'X'
+
+        if me == 'X':
+            score += 1
+            if opp == 'A':
+                score += 3
+            if opp == 'C':
+                score += 6
+        if me == 'Y':
+            score += 2
+            if opp == 'B':
+                score += 3
+            if opp == 'A':
+                score += 6
+        if me == 'Z':
+            score += 3
+            if opp == 'C':
+                score += 3
+            if opp == 'B':
+                score += 6
+        total += score
+
+        print(opp, me, score, total)
+
+    return total
+
 
 
 def main():
