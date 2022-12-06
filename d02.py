d7f1d92f5de37d2144afc4a299493851c4da85cf
Patrick Mendonca <mendonca.patrick@yahoo.com>
2022-12-06 17:29:41 +0800

    day 5
    day 2 game-
    entry works, trying to clean up choices before next play
    no scoring or check for wins yet

--- d02.py
+++ d02.py
@@ -6,8 +6,10 @@ from shared_use import read_file
 
 # create an image cache for quick loading
 image_cache = {}
-# function to pre-load images into dictionary, avoids redundant image load
+
+
 def get_img(key):
+    # load image, will not attempt re-load if already in memory
     if key not in image_cache:
         image_cache[key] = pg.image.load(key)
     return image_cache[key]
@@ -23,7 +25,7 @@ class Elf(pg.sprite.Sprite):
         x_size - the size of the sprite in width
         y_size - the size of the sprite in height
     """
-    def __init__(self, flip=False, boss=False, x_size=200, y_size=200):
+    def __init__(self, enemy=False, boss=False, x_size=200, y_size=200):
         pg.sprite.Sprite.__init__(self)
         # load image, convert for transparent BG
         self.image = get_img('IMG/common/elf.png')
@@ -32,18 +34,17 @@ class Elf(pg.sprite.Sprite):
         # resize img from passed params
         self.image = pg.transform.smoothscale(self.image, (x_size, y_size))
         # if invert flag, flip on x
-        self.enemy = flip
+        self.enemy = enemy
         if self.enemy:
             self.image = pg.transform.flip(self.image, True, False)
-        # define rect for sprite, on load create rect off screen
+        # define rect for sprite, on load create rect off-screen
         self.rect = self.image.get_rect()
         self.rect.center = (-500, -500)
 
         # internal values to control movement
-        self.enter = False # to control walk-up
-        self.newborn = True # for positioning pre-walk-up
-        self.my_choice = None # our choice in game (rock, paper, scissors)
-
+        self.enter = False  # to control walk-up
+        self.newborn = True  # for positioning pre-walk-up
+        self.my_choice = None  # our choice in game (rock, paper, scissors)
 
     def update(self):
         # if we are initializing, show walk-up
@@ -62,7 +63,7 @@ class Elf(pg.sprite.Sprite):
             return 'scissors'
 
     def walk_up(self):
-        # first motion, move to just offscreen
+        # first motion, move to just off-screen
         if self.newborn:
             if self.enemy:
                 self.rect.center = (1100, 700)
@@ -70,23 +71,55 @@ class Elf(pg.sprite.Sprite):
                 self.rect.center = (-100, 700)
             self.newborn = False
 
-        # check if we are walking in right of left
         move_speed = 10
-        if self.enemy: move_speed *= -1
+        # make enemy walk right to left
+        if self.enemy:
+            move_speed *= -1
         self.rect = self.rect.move(move_speed, 0)
 
         # check when to stop
         if self.enemy:
-            if self.rect.left <= 550: self.enter=False
+            if self.rect.left <= 550:
+                self.enter = False
         else:
-            if self.rect.right >= 450: self.enter=False
+            if self.rect.right >= 450:
+                self.enter = False
 
 
 class Rock(pg.sprite.Sprite):
 
-    def __init__(self, size=(200, 200), rot=0):
+    def __init__(self, size=(250, 250), pos=(100, 100), rot=0):
         pg.sprite.Sprite.__init__(self)
+        self.image = get_img('IMG/d02/rock.webp')
+        self.image.convert_alpha()
+        self.image = pg.transform.smoothscale(self.image, size)
+        self.image = pg.transform.rotate(self.image, rot)
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
 
+
+class Paper(pg.sprite.Sprite):
+
+    def __init__(self, size=(250, 250), pos=(100, 100), rot=0):
+        pg.sprite.Sprite.__init__(self)
+        self.image = get_img('IMG/d02/paper.png')
+        self.image.convert_alpha()
+        self.image = pg.transform.smoothscale(self.image, size)
+        self.image = pg.transform.rotate(self.image, rot)
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+
+
+class Scissors(pg.sprite.Sprite):
+
+    def __init__(self, size=(250, 250), pos=(100, 100), rot=0):
+        pg.sprite.Sprite.__init__(self)
+        self.image = get_img('IMG/d02/scissor.webp')
+        self.image.convert_alpha()
+        self.image = pg.transform.smoothscale(self.image, size)
+        self.image = pg.transform.rotate(self.image, rot)
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
 
 
 def process(my_data, part2_flag=0):
@@ -176,9 +209,7 @@ def main_loop():
     screen.blit(loading_img, loading_rect)
     pg.display.update()
 
-
-
-    # define sprite gorups
+    # define sprite groups
     all_sprites = pg.sprite.RenderUpdates()
     player_group = pg.sprite.RenderUpdates()
     enemy_group = pg.sprite.RenderUpdates()
@@ -188,61 +219,24 @@ def main_loop():
     # player sprite
     player = Elf()
     # first enemy sprite
-    enemy = Elf(True)
+    enemy = Elf(enemy=True)
     # we are entering
     player.enter = True
     enemy.enter = True
+
     # button sprites
-    rock_button = pg.sprite.Sprite()
-    rock_button.image = get_img('IMG/d02/rock.webp')
-    rock_button.image.convert_alpha()
-    rock_button.image = pg.transform.smoothscale(rock_button.image, (250, 250))
-    rock_button.rect = rock_button.image.get_rect()
-    rock_button.rect.center = (200, 300)
-    paper_button = pg.sprite.Sprite()
-    paper_button.image = get_img('IMG/d02/paper.png')
-    paper_button.image.convert_alpha()
-    paper_button.image = pg.transform.smoothscale(paper_button.image, (250, 250))
-    paper_button.rect = paper_button.image.get_rect()
-    paper_button.rect.center = (500, 300)
-    scis_button = pg.sprite.Sprite()
-    scis_button.image = get_img('IMG/d02/scissor.webp')
-    scis_button.image.convert_alpha()
-    scis_button.image = pg.transform.smoothscale(scis_button.image, (250, 250))
-    scis_button.rect = scis_button.image.get_rect()
-    scis_button.rect.center = (800, 300)
+    rock_button = Rock((250, 250), (200, 300), 0)
+    paper_button = Paper((250, 250), (500, 300), 0)
+    scis_button = Scissors((250, 250), (800, 300), 0)
 
-
-    # play sprites
-    rock_play = pg.sprite.Sprite()
-    rock_play.image = get_img('IMG/d02/rock.webp')
-    rock_play.image.convert_alpha()
-    rock_play.image = pg.transform.smoothscale(rock_play.image, (150, 150))
-    rock_play.image = pg.transform.rotate(rock_play.image, 30)
-    rock_play.rect = rock_play.image.get_rect()
-    paper_play = pg.sprite.Sprite()
-    paper_play.image = get_img('IMG/d02/paper.png')
-    paper_play.image.convert_alpha()
-    paper_play.image = pg.transform.smoothscale(paper_play.image, (150, 150))
-    paper_play.image = pg.transform.rotate(paper_play.image, 30)
-    paper_play.rect = paper_play.image.get_rect()
-    scis_play = pg.sprite.Sprite()
-    scis_play.image = get_img('IMG/d02/scissor.webp')
-    scis_play.image.convert_alpha()
-    scis_play.image = pg.transform.smoothscale(scis_play.image, (150, 150))
-    scis_play.image = pg.transform.rotate(scis_play.image, 30)
-    scis_play.rect = scis_play.image.get_rect()
-
-
-    all_sprites.add(player, enemy)
     button_group.add(rock_button, paper_button, scis_button)
     player_group.add(player)
     enemy_group.add(enemy)
+    all_sprites.add(player_group, enemy_group)
 
     # loop and game control variables
     running = True  # game loop control flag
     new_enemy = False    # spawn a new enemy?
-    make_choice = False     # ready to choose play
     click_list = []     # event list on mouse click
 
     # clear screen for game loop animation
@@ -261,7 +255,6 @@ def main_loop():
             if event.type == pg.MOUSEBUTTONUP:
                 click_list = [s for s in button_group if s.rect.collidepoint(event.pos)]
 
-
         # if enemy needed, create it
         if new_enemy:
             print('Creating new enemy')
@@ -279,7 +272,6 @@ def main_loop():
         if not enemy.enter:
             # reset previous choice, if any
             player_choice = None
-            computer_choice = None
             all_sprites.add(button_group)
 
             # if the mouse has been click, check if it made a choice
@@ -295,20 +287,36 @@ def main_loop():
             if player_choice:
                 computer_choice = enemy.choose()
 
+                print(player_choice, computer_choice)
+
                 # after the computer chooses, show the play
                 # where play icons will be centered
                 p1_choice_pos = (475, 650)
                 p2_choice_pos = (525, 650)
-                # add choices to group
+                choice_size = (100, 100)
+                choice_rot = 30
+                # define player icons
                 if player_choice == 'rock':
+                    p1_icon = Rock(choice_size, p1_choice_pos, choice_rot)
+                elif player_choice == 'paper':
+                    p1_icon = Paper(choice_size, p1_choice_pos, choice_rot)
+                else:
+                    p1_icon = Scissors(choice_size, p1_choice_pos, choice_rot)
+                if computer_choice == 'rock':
+                    p2_icon = Rock(choice_size, p2_choice_pos, -choice_rot)
+                elif player_choice == 'paper':
+                    p2_icon = Paper(choice_size, p2_choice_pos, -choice_rot)
+                else:
+                    p2_icon = Scissors(choice_size, p2_choice_pos, -choice_rot)
 
-                    player_group.add()
-
+                player_group.add(p1_icon)
+                enemy_group.add(p2_icon)
+                all_sprites.add(player_group, enemy_group)
 
         # update sprite groups
         all_sprites.update()
-        player_group.update()
-        enemy_group.update()
+        # player_group.update()
+        # enemy_group.update()
 
         # render
         all_sprites.clear(screen, background)
@@ -320,8 +328,6 @@ def main_loop():
 
         # frame rate control
         clock.tick(60)
-
-
 
     # outside main game loop, clean up and exit
     pg.quit()
